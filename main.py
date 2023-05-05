de4ad2c4dab6cb9092e30256fa22c23a42617f41
Anna <annacormio01@gmail.com>
2023-05-05 09:36:13 +0200

    done

--- main.py
+++ main.py
@@ -25,39 +25,44 @@ def overlap(s1, s2):
     return over
 
 
+#CHOOSE PAIR
+def maxPair(l):
+    if len(l) == 1:  # only one pair with max overlap length
+        p = l[0]  # extract the only pair from the list
+    else:  # if there are more pairs with that overlap max length
+        p = random.choice(l)  # choose one randomly within the list
+    return p
+
+#MERGE
+def merge(l): #merge all elements in a list one after the other
+    merge = ''
+    for i in l:
+        merge += i  # merge all remaining sequences together
+    return merge  # return merged sequence
 
 
 
+#COMPUTE SHORTES COMMON SUPERSTRING
 def SCS(l):  #l is the list of subsequences
-    if len(l) == 1:
+    if len(l) == 1: #only one seq in the list
         return l[0]
-    else:
+    else: #more than one seq in the list
         l_max = 0  # var for the max length of the computed overlap
-        o_max = ''  # variable for the max overlap sequence
         max_pairs = []
         for pair in itertools.permutations(l, 2): #iter over each pair
-            o = overlap(pair[0], pair[1])  # compute overlap of 1st seq suffix and 2nd seq prefix of the pair
+            o = overlap(pair[0], pair[1])  # compute overlap of 1st seq suffix and 2nd seq prefix of the pair --> function defined above
             if len(o) > l_max:  # if the length of the computed pair overlap is greater than the stored max length up to that point
                 l_max = len(o)  # the max length is updated
                 max_pairs.clear() #removes all elements from  a list when we find a new pair which has greater length than the one stored before
                 max_pairs.append(pair) #append the max found
 
             elif len(o) == l_max:  # if 2 pairs have the same overlap length
-                max_pairs.append(pair) #append the other pair with same max overlap length to the listù
+                max_pairs.append(pair) #append the other pair with same max overlap length to the list
 
-        #print(f'max pair list   {max_pairs}')
-        if len(max_pairs) == 1: #only one pair with max overlap length
-            p = max_pairs[0] #extract the only pair from the list
-            suf = p[0]
-            pre = p[1]
-            o_max = overlap(suf,pre)  # overlap sequence
-
-        else: #if there are more pairs with that overlpa max length
-            p=random.choice(max_pairs) #choose one randomly within the list
-            #print(f'random choice is {p}')
-            suf= p[0]
-            pre=p[1]
-            o_max = overlap(suf,pre)
+        p=maxPair(max_pairs) #choose pair among the built list --> function defined above
+        suf = p[0]  # seq with suffix overlap
+        pre = p[1]  # seq with prefix ovverlap
+        o_max = overlap(suf, pre)  # overlap sequence
 
         if o_max: #if we have an overlap in the seq
             l.remove(suf)
@@ -69,16 +74,14 @@ def SCS(l):  #l is the list of subsequences
             return SCS(l) #recursive call
 
         else: #if no pair combination overlaps
-            merge = ''
-            for i in l:
-                merge += i   #merge all remaining sequences together
-            return merge
+            m = merge(l) #function defined above
+            return m
 
-# MAIN
-DNA = getGenome(15)
-#gen='TAGTAGCGATCA'
-subseq = getSubstrings(DNA, 6)
-#subseq= ['AGTACG','GTACGT','CCCGAT', 'GACTGG', 'AACGCC','GACCTT']
-print(DNA)
-print(subseq)
-print(SCS(subseq))
+
+if __name__ == "__main__": #main
+    DNA = getGenome(15)
+    subseq = getSubstrings(DNA, 6)
+    print(DNA)
+    print(subseq)
+    scs=SCS(subseq)
+    print(scs)
